4fdc199cf03a023f22a539ca02e9e5fa976a598a
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-05-15 01:35:52 +0600

    IT WORKED!!!!!!!!!!!!

---
+++ parent/loadProject.js
@@ -0,0 +1,4 @@
+let reactAppScriptElement = document.createElement("script");
+reactAppScriptElement.src = `${baseUrl}/react-app.js`; // baseurl comes from main src
+
+document.body.appendChild(reactAppScriptElement);
